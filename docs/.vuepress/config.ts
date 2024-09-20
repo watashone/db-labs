825d9e4a81a4048cc51a47bc6130e2581d0b1f65
dk872 <dimakulyk2005@gmail.com>
2024-09-20 20:57:03 +0300

    'deploy'

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,11 +4,11 @@ import MarkdownItPlantuml from 'markdown-it-plantuml';
 
 export default defineUserConfig({
   // Замінити на назву свого репозиторія
-  base: "/db-course-hope-template/",
+  base: "/db-labs/",
 
   lang: "en-US",
   // Замінити на назву свого проєкту
-  title: "Назва проєкту",
+  title: "Система управління відкритими даними",
   description: "Лабораторні роботи",
 
   theme,
